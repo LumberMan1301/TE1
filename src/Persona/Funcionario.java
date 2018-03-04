b5522c756bddddaf7ef6f45fe34c6eb74af9b77a
Mariano <marianomm@rocketmail.com>
2018-03-04 00:37:43 -0600

    00.36.4.3.2018 Se comienza con JavaDoc

--- src/Persona/Funcionario.java
+++ src/Persona/Funcionario.java
@@ -1,13 +1,28 @@
 package Persona;
 
-
+/**
+ * la siguiente es una clase derivada de la clase Persona, podemos observar que se esta empleando la herencia
+ * mediante la palabra reservada "extends", la cual sirve para referenciar que la clase Funcionario es Hija
+ * de la clase Persona
+ */
 
 
 public class Funcionario extends Persona {
+    /**
+     * atributos mas relevantes de la clase Funcionario
+     * el atributo "codFuncionario" es estatico debido a que necesitamos que el valor se guarde aun despues de
+     * terminarse de ejecutar el metodo constructor
+     */
     private String puesto;
     private static int codFuncionario = 0000;
 
-
+    /**
+     * Constructor de la Clase Funcionario
+     * @param nombre
+     * @param sexo
+     * @param edad
+     * @param puesto
+     */
     public Funcionario(String nombre, String sexo, int edad, String puesto) {
         super(nombre, sexo, edad);
         this.puesto = puesto;
--- src/Persona/Persona.java
+++ src/Persona/Persona.java
@@ -1,11 +1,44 @@
 package Persona;
 
+/**
+ * <p>
+ * La siguiente es una clase demostrativa, para representar algunos de los conceptos basicos de POO,
+ * en este preograma se van a representar lso conceptos de:
+ * Abstracccion, Encapsulacion, Clase, Atributo, Metodo, Instancia, Herencia, Polimorfismo, Override, Overload, y Modularidad
+ * </p>
+ * <p>
+ *     Se presenta el contexto en el cual se ambienta la implementacion de la siguiente solucion:
+ *     Una entidad de enseñanza desea llevar el control tanto de funcionarios como de estudiantes, mediante la creacion
+ *     de archivos(Personas) con caracteristicas similares, pero que el sistema logre reconocer que son entidades separadas.
+ * </p>
+ */
+
+
+
+/**
+ * La siguiente es la Super-clase Persona, aqui podemos apreciar el concepto de Abstraccion, ya que determinamos
+ * las caracteristicas mas reelevantes de el objeto persona, adapatdos para este contexto en especial, asi mismo
+ * se abarca el concepto de clase, ya que la construccion de la clase viene de la abstraccion del objeto.
+ * La clase es de tipo abstracta para que no se puedan crear instancias de ella, sino que solo se ´puedan crear
+ * instancias de sus clases derivadas
+ */
 public abstract class Persona {
+    /**
+     * Se declaran las varibles de nuestra clase padre, en este punto podemos apreciar los atributos de el Objeto Persona
+     * que son el resultado de la Abstraccion, para este contexto en especifico del objeto nada mas se ocuparon los atributos
+     * nombre, sexo y edad.
+     */
     protected String nombre;
     protected String sexo;
     protected int edad;
 
-
+    /**
+     * Se presenta el constructor de la clase, el cual es un metodo especial para inicializar las instancias
+     *
+     * @param nombre
+     * @param sexo
+     * @param edad
+     */
     public Persona(String nombre, String sexo, int edad) {
         this.nombre = nombre;
         this.sexo = sexo;
@@ -13,9 +46,20 @@ public abstract class Persona {
         System.out.println("La persona "+ this.nombre + " ha sido creada con exito");
     }
 
+    /**
+     * metodo publico para mostrar la informacion almacenada en una intancia
+     * la encapsulacion se relaciona con la palabra clave de ocultar, en este metodo podemos observar coomo
+     * se oculta la implementacion de mostrar la informacion, llamando de un metodo publico a un metodo privado,
+     * al metodo publico se puede acceder facilmente, mas en cambio al metodo privado solo se puede acceder por
+     * medio del metodo publico.
+     */
     public void mostrarInformacion(){
             mostrarInformacionP();
         }
+
+    /**
+     * metodo privado que contiene la logica.
+     */
     private void mostrarInformacionP(){
         System.out.println("Nombre: "+this.nombre);
         System.out.println("Sexo: "+ this.sexo);
